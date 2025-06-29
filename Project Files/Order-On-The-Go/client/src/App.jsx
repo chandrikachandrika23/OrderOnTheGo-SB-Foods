d239121aa7a7ec4d20b3aa1b5fc12b27c18b2722
Chandrika Rajulapati <chandrikarajulapati317@gmail.com>
2025-06-29 19:15:07 +0530

    Add files via upload

---
+++ Project Files/Order-On-The-Go/client/src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
